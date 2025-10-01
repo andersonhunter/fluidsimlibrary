09394ba0ceb0e36423b2109095f3bc66fbc027e2
Hunter Anderson <129843902+andersonhunter@users.noreply.github.com>
2025-10-01 20:04:30 +0000

    created function definitions

---
+++ fluid_formulae.h
@@ -0,0 +1,55 @@
+#ifndef FLUID_FORMULAE_H_
+#define FLUID_FORMULAE_H_
+
+// Define constants
+extern const float GRAVITY;   // Acceleration due to gravity
+extern const float SG_WATER;  // Specific gravity of water
+
+float forceMA(float mass, float acceleration);
+
+float weightMA(float mass);
+
+float absolutePressure(float atmosphericPressure, float vacuumPressure);
+
+float density(float mass, float volume);
+
+float specificWeightWV(float weight, float volume);
+
+float specificWeightDensity(float density);
+
+float specificVolumeSpecificWeight(float specificWeight);
+
+float specificVolumeWeightVolume(float weight, float volume);
+
+float specificVolumeDensity(float density);
+
+float specificGravity(float density);
+
+float surfaceTension(float attraction, float length);
+
+float vaporSolidInterfaceSurfaceTension(
+    float liquidSolidSurfaceTension,
+    float vaporLiquidSurfaceTension,
+    float contactAngle
+);
+
+float columnarWeight(float fluidSpecificWeight, float height, float diameter);
+
+float forceSurfaceTension(float surfaceTension, float diameter);
+
+float columnHeight(
+    float surfaceTension,
+    float contactAngle,
+    float specificWeight,
+    float diameter
+);
+
+float stress(float force, float area);
+
+float shearStress(float viscosityCoefficient, float velocity, float distance);
+
+float stokesLaw(float outsideRadius, float viscosity, float sphereSW, float fluidSW);
+
+float kinematicViscosity(float viscosity, float density);
+
+#endif
