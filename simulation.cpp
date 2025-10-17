0b45654ce4754722f714fc45368ac693c1b7b73c
Hunter Anderson <129843902+andersonhunter@users.noreply.github.com>
2025-10-17 18:19:35 +0000

    defined point, set up grid

--- simulation.cpp
+++ simulation.cpp
@@ -2,6 +2,26 @@
 #include <stdio.h>
 #include "fluid_formulae.h"
 
-int main() {
+#ifndef SIZE
+#define SIZE 16
+#endif
+
+struct Point {
+    // A point within the grid which holds different quantities
+    float pressure;
+    float temperature;
+};
+
+int main(int argc, char* argv[]) {
+    
+    // Initialize grid of points
+    struct Point* grid = (struct Point *)malloc(SIZE * sizeof(Point));
+
+    // Set initial values
+    for(int i = 0; i < SIZE; i++) {
+        grid[i].pressure = 0;
+        grid[i].temperature = 0;
+    }
+
     return 0;
 }
