4efcc2fad32ce3b5f9a6bdbeb99c45954ee10afc
Hunter Anderson <129843902+andersonhunter@users.noreply.github.com>
2025-10-20 18:49:04 +0000

    added vx vy setters, redoing advection interpolation

--- simulation.cpp
+++ simulation.cpp
@@ -18,6 +18,12 @@ struct Point {
     float density = 1.;      // Density
 };
 
+// Function prototypes
+void calculateAdvection(struct Point* grid, float timestep);
+struct Point getAtIndex(int x, int y, struct Point* grid);
+void setVxAtIndex(int x, int y, float vx, struct Point* grid);
+void setVyAtIndex(int x, int y, float vy, struct Point* grid);
+
 void calculateAdvection(struct Point* grid, float timestep) {
     // Use the MacCormack Method
     // Do a forward pass to estimate advection at each point using the Semi-Lagrangian method
@@ -31,29 +37,39 @@ void calculateAdvection(struct Point* grid, float timestep) {
     //  Corrected Value = Forward Estimate + 0.5 * (Original Value - Backward Estimate)
     
     //TODO: Maybe add a minmod limiter or filtering to prevent oscillations
-    float forwardX, forwardY;
+    // Currently doing bicubic interpolation for the 4x4 grid [i - 1, j - 1] to [i + 2, j + 2]
+    // Bilinear would be more performant but less accurate
     for(int x = 0; x < SIZE; x++) {
         // Need to add boundary checking
         for(int y = 0; y < SIZE; y++) {
+            float forwardX, forwardY;
+            float backwardX, backwardY;
             // Calculate the forward estimate
             // Get previous (x, y) coords
             float xPrev = (float)x - getAtIndex(x, y, grid).vx * timestep;
             float yPrev = (float)y - getAtIndex(x, y, grid).vy * timestep;
-            // Calculate fractional offsets (amount each point differs from its nearest point)
+            // Clamp to nearest valid index and sanity check
             int i = floor(xPrev);
             int j = floor(yPrev);
+            if (i < 0) {
+                i = 0;
+            }
+            if (i >= SIZE) {
+                i = SIZE - 1;
+            }
+            if (j < 0) {
+                j = 0;
+            }
+            if (j >= SIZE) {
+                j = SIZE - 1;
+            }
+            // Calculate fractional offsets
             float dx = xPrev - i;
             float dy = yPrev - j;
-            // Forward interpolate x velocity
-            forwardX = (1 - dx) * (1 - dy) * getAtIndex(i, j, grid).vx;
-            forwardX += (dx * (1 - dy) * getAtIndex(i + 1, j, grid).vx);
-            forwardX += ((1 - dx) * dy * getAtIndex(i, j + 1, grid).vx);
-            forwardX += (dx * dy * getAtIndex(i + 1, j + 1, grid).vx);
-            // Forward interpolate y velocity
-            forwardY = (1 - dx) * (1 - dy) * getAtIndex(i, j, grid).vy;
-            forwardY += (dx * (1 - dy) * getAtIndex(i + 1, j, grid).vy);
-            forwardY += ((1 - dx) * dy * getAtIndex(i, j + 1, grid).vy);
-            forwardY += (dx * dy * getAtIndex(i + 1, j + 1, grid).vy);
+            // Forward interpolate x and y velocities
+            // Add more quantities (temperature, density, etc) here
+            // Maybe turn forward into a forwardStruct at some point?
+            
         }
     }
 }
@@ -64,12 +80,20 @@ struct Point getAtIndex(int x, int y, struct Point* grid) {
     return grid[SIZE * x + y];
 }
 
+void setVxAtIndex(int x, int y, float vx, struct Point* grid) {
+    // Set x-velocity at a given index
+    grid[SIZE * x + y].vx = vx;
+}
+
+void setVyAtIndex(int x, int y, float vy, struct Point* grid) {
+    grid[SIZE * x + y].vy = vy;
+}
+
 int main(int argc, char* argv[]) {
     
     // Initialize grid of points
     // Pseudo-2D array, index in with pointer arithmetic grid[SIZE * row + column]
     struct Point* grid = (struct Point *)malloc((SIZE * SIZE) * sizeof(Point));
-
     free(grid);
     return 0;
 }
