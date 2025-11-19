b388322b4c849fcc22783987a8065487417f2d26
Hunter Anderson <129843902+andersonhunter@users.noreply.github.com>
2025-11-19 01:25:17 +0000

    i dont wanna talk about it ok

--- fluidSim.cpp
+++ fluidSim.cpp
@@ -1643,9 +1643,8 @@ void calculateAdvection(float timestep) {
 					}
 				}
 			}
-		}
-		// Update ghost cells
-			for (int i = 0; i < SIZE - 1; i++) {
+			// Update ghost cells
+			for (int i = 0; i <= SIZE - 1; i++) {
 				// Do the top and bottom rows
 				tempPressures[i] = tempPressures[SIZE + i];
 				tempPressures[SIZE * (SIZE - 1) + i] = tempPressures[SIZE * (SIZE - 2) + i];
@@ -1653,6 +1652,7 @@ void calculateAdvection(float timestep) {
 				tempPressures[SIZE * i] = tempPressures[SIZE * i + 1];
 				tempPressures[SIZE * i + (SIZE - 1)] = tempPressures[SIZE * i + (SIZE - 2)];
 			}
+		}
 
 		// Now correct x- and y-velocities to account for pressure buildup (or lack thereof)
 		// vx = (temp vx) - ((dt / rho) * (Pright - Pleft) / 2 * CELLSIZE)
